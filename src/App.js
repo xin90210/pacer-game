2240c34ec069a8ad7b57c40e022031d9f6402d2c
xin90210 <xin90210@gmail.com>
2020-03-03 18:22:34 +0200

    css modules added

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
-function App() {
+const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-          <img src="https://is4-ssl.mzstatic.com/image/thumb/Purple113/v4/73/00/4c/73004c31-75c9-a49b-2a40-e40b7894b3ec/source/512x512bb.jpg" alt="parrot"/>
-      </header>
-      <nav className='nav'>
-          <div><a href="">Profile</a></div>
-          <div><a href="">Messages</a></div>
-          <div><a href="">News</a></div>
-          <div><a href="">User</a></div>
-          <div><a href="">Settings</a></div>
-      </nav>
-      <div className='content'>
-          <img src="https://previews.123rf.com/images/hellokisdottir/hellokisdottir1505/hellokisdottir150500030/40086874-summer-forest-flat-background-simple-and-cute-landscape-for-your-design.jpg" alt=""/>
-
-      <div>
-        ava + description
-      </div>
-      <div>
-        my posts
-        <div>
-          new post
-        </div>
-      </div>
-      <div>
-        <div>
-          post 1
-        </div>
-        <div>
-          post2
-        </div>
-      </div>
-    </div>
-    </div>
-  );
+      <Header />
+      <Navbar />
+      <Profile />
+    </div>);
 }
-
-
-
 
 export default App;
